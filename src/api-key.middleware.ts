8a87be5ede313cef32c676fc1772f1e2bd7a2ace
patrykjachym <jachympatryk99@gmail.com>
2024-04-24 18:53:09 +0200

    app supdate

--- src/api-key.middleware.ts
+++ src/api-key.middleware.ts
@@ -5,7 +5,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class ApiKeyMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction): void {
-    const apiKey = req.headers['x-api-key'];
+    const apiKey = req.headers['X-api-key'];
     const validApiKey = process.env.API_KEY;
 
     if (apiKey === validApiKey) {
